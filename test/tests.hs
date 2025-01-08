7de63a8b1c51520950eacd6f7c93ed1eb6a76ec6
Mario Lang <mlang@blind.guru>
2025-01-08 22:04:40 +0100

    zipWith

--- test/tests.hs
+++ test/tests.hs
@@ -21,9 +21,9 @@ polyglot = testGroup "Polyglot" [
 
 perftSuite :: TestTree
 perftSuite = testGroup "PerftSuite" $ map f info where
-  f (fen, ds) = testGroup fen $ map g (zip [1..] ds) where
-    g (n, d) = testCase ("Depth=" ++ show n ++ ", Nodes=" ++ show d) $
-               perft n <$> fromFEN fen @?= Just d
+  f (fen, ds) = testGroup fen $ zipWith g [1..] ds where
+    g n d = testCase ("Depth=" ++ show n ++ ", Nodes=" ++ show d) $
+            perft n <$> fromFEN fen @?= Just d
   info = [
     ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", [20, 400, 8902, 197281, 4865609, 119060324]),
     ("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", [48, 2039, 97862, 4085603, 193690690]),
